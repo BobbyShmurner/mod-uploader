f045f91bb9c1659cc11ac070eece5338239f7df4
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2022-03-03 19:38:11 +0000

    Some Basic Logging

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const fs = require('fs');
+
 try {
 	const qmodUrl = core.getInput('qmod');
+	const modJsonPath = core.getInput('mod-json')
+	const modJson = JSON.parse(fs.readFileSync(modJsonPath))
+
 	console.log(`QMod URL: ${qmodUrl}`);
+	console.log(`mod.json Path: ${modJsonPath}`);
+	console.log(`mod.json: ${modJson}`);
 } catch (error) {
 	core.setFailed(error.message);
 }
