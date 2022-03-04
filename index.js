c046471a9ab094faccffbeb6490b0adc9f3ca903
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2022-03-04 17:21:43 +0000

    Fork Test

--- index.js
+++ index.js
@@ -16,19 +16,29 @@ async function main() {
 	console.log("Getting Fork of Mod Repo");
 
 	var modRepo;
+	var hasForked = false;
+	var timeoutTime = new Date().getTime() + (60 * 1000);
 
-	try {
-		modRepo = await octokit.rest.repos.get({
-			owner: github.context.repo.owner,
-			repo: "QuestModRepo"
-		});
-	} catch (error) {
-		console.log("Failed to find fork, forking now...");
+	while (true) {
+		try {
+			modRepo = await octokit.rest.repos.get({
+				owner: "sc2ad",
+				repo: "TetrLang"
+			});
 
-		await octokit.rest.repos.createFork({
-			owner: "BobbyShmurner",
-			repo: "QuestModRepo"
-		});
+			break;
+		} catch (error) {
+			if (timeoutTime < Date().getTime()) throw "Times out when creating a fork of the Mod Repo. Maybe try creating the fork manually?";
+			if (hasForked) continue;
+
+			console.log("Failed to find fork, forking now...");
+			hasForked = true;
+
+			await octokit.rest.repos.createFork({
+				owner: "BobbyShmurner",
+				repo: "QuestModRepo"
+			});
+		}
 	}
 
 	console.log(modRepo.url);
