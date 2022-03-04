ab2c5575d31357b03878049d7388aebc975e1403
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2022-03-04 16:44:05 +0000

    Move code into async function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 const fs = require('fs');
 
-try {
+async function main() {
 	const qmodUrl = core.getInput('qmod');
 	const modJsonPath = core.getInput('mod-json');
 	const modJson = JSON.parse(fs.readFileSync(modJsonPath));
@@ -19,6 +19,10 @@ try {
 		owner: "BobbyShmurner",
 		repo: "QuestModRepo"
 	});
+}
+
+try {
+	main();
 } catch (error) {
 	core.setFailed(error.message);
 }
