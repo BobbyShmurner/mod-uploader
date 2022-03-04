f4ab231f90ff082db5afa75f9bcbfcf01ae9ef43
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2022-03-04 16:37:37 +0000

    Forks Mod Repo

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const fs = require('fs');
 
 try {
 	const qmodUrl = core.getInput('qmod');
-	const modJsonPath = core.getInput('mod-json')
-	const modJson = JSON.parse(fs.readFileSync(modJsonPath))
+	const modJsonPath = core.getInput('mod-json');
+	const modJson = JSON.parse(fs.readFileSync(modJsonPath));
 
-	console.log(`QMod URL: ${qmodUrl}`);
-	console.log(`mod.json Path: ${modJsonPath}`);
-	console.log(`mod.json: ${JSON.stringify(modJson, null, 4)}`);
+	console.log("Getting Octokit");
+
+	const gitToken = core.getInput('token');
+	const octokit = github.getOctokit(gitToken);
+
+	console.log("Forking Mod Repo");
+
+	await octokit.rest.repos.createFork({
+		owner: "BobbyShmurner",
+		repo: "QuestModRepo"
+	});
 } catch (error) {
 	core.setFailed(error.message);
 }
