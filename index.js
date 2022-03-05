1e46ef71081f62230032d67cc5452fe1470f9597
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2022-03-05 15:28:23 +0000

    Trying to fix throws inside of async function

--- index.js
+++ index.js
@@ -8,61 +8,65 @@ const shell = require('shelljs')
 const semver = require('semver')
 
 async function main() {
-	const modJsonPath = core.getInput('mod-json');
-
-	throw "test :)";
-
-	if (!fs.existsSync(modJsonPath)) {
-		throw `File "${modJsonPath}" does not exist`;
-	}
-
-	const modJson = JSON.parse(fs.readFileSync(modJsonPath));
-	var notes = [];
-
-	core.info("Getting Octokit");
-
-	core.info("Getting Fork of Mod Repo");
-
-	var modRepo;
-	var hasForked = false;
-	var timeoutTime = (new Date()).getTime() + (60 * 1000);
-
 	try {
-		modRepo = (await octokit.rest.repos.get({
-			owner: github.context.repo.owner,
-			repo: "QuestModRepo"
-		})).data;
-	} catch {
-		throw "Failed to find fork of the Mod Repo. Please make sure a fork of the repo exists. You can find the repo here: https://github.com/BigManBobby/QuestModRepo";
-	}
+		const modJsonPath = core.getInput('mod-json');
 
-	if (!modRepo.fork) {
-		throw `${modRepo.html_url} is not a fork of https://github.com/BigManBobby/QuestModRepo`;
-	}
+		throw "test :)";
 
-	core.info("Cloning fork");
-	shell.exec(`git clone ${modRepo.html_url}`);
-
-	core.info("Getting the repo's mods");
-	const repoMods = JSON.parse(fs.readFileSync("QuestModRepo/mods.json"));
-
-	core.info("Adding mod entry to mod repo");
-
-	if (!repoMods.hasOwnProperty(modJson.packageVersion)) {
-		if (semver.valid(modJson.packageVersion)) {
-			repoMods[modJson.packageVersion] = [];
-
-			const msg = `There were no mods found for the version ${modJson.packageVersion}, so a new verion entry was created`;
-
-			core.warning(msg);
-			notes.push(msg);
-		} else {
-			throw `Version ${modJson.packageVersion} is invalid!`;
+		if (!fs.existsSync(modJsonPath)) {
+			throw `File "${modJsonPath}" does not exist`;
 		}
-	}
 
-	repoMods[modJson.packageVersion].push(ConstructModEntry(modJson, modRepo));
-	core.info(JSON.stringify(repoMods, null, 4));
+		const modJson = JSON.parse(fs.readFileSync(modJsonPath));
+		var notes = [];
+
+		core.info("Getting Octokit");
+
+		core.info("Getting Fork of Mod Repo");
+
+		var modRepo;
+		var hasForked = false;
+		var timeoutTime = (new Date()).getTime() + (60 * 1000);
+
+		try {
+			modRepo = (await octokit.rest.repos.get({
+				owner: github.context.repo.owner,
+				repo: "QuestModRepo"
+			})).data;
+		} catch {
+			throw "Failed to find fork of the Mod Repo. Please make sure a fork of the repo exists. You can find the repo here: https://github.com/BigManBobby/QuestModRepo";
+		}
+
+		if (!modRepo.fork) {
+			throw `${modRepo.html_url} is not a fork of https://github.com/BigManBobby/QuestModRepo`;
+		}
+
+		core.info("Cloning fork");
+		shell.exec(`git clone ${modRepo.html_url}`);
+
+		core.info("Getting the repo's mods");
+		const repoMods = JSON.parse(fs.readFileSync("QuestModRepo/mods.json"));
+
+		core.info("Adding mod entry to mod repo");
+
+		if (!repoMods.hasOwnProperty(modJson.packageVersion)) {
+			if (semver.valid(modJson.packageVersion)) {
+				repoMods[modJson.packageVersion] = [];
+
+				const msg = `There were no mods found for the version ${modJson.packageVersion}, so a new verion entry was created`;
+
+				core.warning(msg);
+				notes.push(msg);
+			} else {
+				throw `Version ${modJson.packageVersion} is invalid!`;
+			}
+		}
+
+		repoMods[modJson.packageVersion].push(ConstructModEntry(modJson, modRepo));
+		core.info(JSON.stringify(repoMods, null, 4));
+	} catch (error) {
+		core.setFailed(error);
+	}
 }
 
 function ConstructModEntry(modJson, modRepo) {
@@ -94,8 +98,4 @@ function ConstructModEntry(modJson, modRepo) {
 	return modEntry;
 }
 
-try {
-	main();
-} catch (error) {
-	core.setFailed(error.message);
-}
+main();
